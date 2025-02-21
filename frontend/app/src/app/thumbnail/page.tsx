b0792c02ec437b5448a8f4da81c8a09e051131b6
takumi-goto <tgoto@eviry.com>
2025-02-21 14:20:12 +0900

    fix

--- frontend/app/src/app/thumbnail/page.tsx
+++ frontend/app/src/app/thumbnail/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import ThumbnailAnalyzer from "../components/ui/thumbnailAnalyzer/ThumbnailAnalyzer";
+import { Container } from '@mantine/core';
 
 export default function ThumbnailPage() {
+  const props = {
+    h: 50,
+    mt: 'xl',
+  };
   return (
-    <div className="p-4">
+    <Container fluid {...props}>
       <ThumbnailAnalyzer />
-    </div>
+    </Container>
   );
 }
